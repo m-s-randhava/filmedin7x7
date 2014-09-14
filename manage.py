61a3ccd347401861de0328fe0c663e3ce441cce2
m-s-randhava <mohan.randhava@gmail.com>
2014-09-14 16:33:20 -0700

    Change run server to run 0.0.0.0

--- manage.py
+++ manage.py
@@ -4,11 +4,15 @@ from app import create_app, setup_app
 from flask.ext.script import Manager, Shell
 
 env_configuration = os.getenv('FLASK_CONFIG') if os.getenv('FLASK_CONFIG') else 'default'
+env_configuration = 'development'
 print os.getenv('FLASK_CONFIG')
-app = create_app(env_configuration)
-print "Running the " + env_configuration + " configuration ..."
 
+app = create_app(env_configuration)
+
+print "Running the " + env_configuration + " configuration ..."
 setup_app(env_configuration)
+print "Finished running the " + env_configuration + " configuration ..."
+
 manager = Manager(app)
 
 def make_shell_context():
